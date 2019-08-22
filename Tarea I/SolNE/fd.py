bc360363f8f0bca385ac88d9987e558eccbd1a46
Jasson Rodríguez <jassonrm@icloud.com>
2019-08-22 16:06:15 -0600

    Funciones sin derivada Python

--- Tarea I/SolNE/fd.py
+++ Tarea I/SolNE/fd.py
@@ -77,9 +77,72 @@ def sne_fd_5(valorInicial,anterior, tolerancia,funcion,graf = 1):
         plot_f(errors,puntos)
 
 
+# Jain's method
+def sne_fd_6(x0, tol, funcion, graf=1):
+    variable = Symbol("x")
+    funcion = sympify(funcion)
+    iteracion = 0
+    f = lambdify(variable, funcion, "numpy")
+    x = x0
+    error = abs(f(x))
+    errors = [error]
+    while error > tol and iteracion < 1000:
+        denominadory = (f(x + f(x)) - f(x))
+        if denominadory == 0:
+            break;
+        y = x - (f(x) ** 2) / denominadory
+        denominadorx = ((f(x + f(x)) - f(x)) * (f(x) - f(y)))
+        if denominadorx == 0:
+            break;
+        x = x - (f(x) ** 3) / denominadorx
+        iteracion += 1
+        error = abs(f(x))
+        errors.append(error)
+    if (graf != 0 and graf != 1):
+        print("WARNING: graf has two possible values, 1 or 0")
+    elif (graf == 1):
+        plot(errors, "Jain")
+    return x, iteracion
+
+# Zhang's method
+def sne_fd_7(x0, gamma, tol, funcion, graf=1):
+    variable = Symbol("x")
+    funcion = sympify(funcion)
+    iteracion = 0
+    f = lambdify(variable, funcion, "numpy")
+    x = x0
+    error = abs(f(x))
+    errors = [error]
+
+    def divDif1(x, y):
+        return (f(x) - f(y)) / (x - y)
+
+    def divDif2(x, y, z):
+        return (divDif1(x, y) - divDif1(y, z)) / (x - z)
+
+    while error > tol and iteracion < 1000:
+        w = x + gamma * f(x)
+        denominadory = divDif1(x, w)
+        if denominadory == 0:
+            break;
+        y = x - f(x) / denominadory
+        denominadorx = divDif1(x, y) + (y - x) * divDif2(x, w, y)
+        if denominadorx == 0:
+            break;
+        x = y - f(y) / denominadorx
+
+        iteracion += 1
+        error = abs(f(x))
+        errors.append(error)
+    if (graf != 0 and graf != 1):
+        print("WARNING: graf has two possible values, 1 or 0")
+    elif (graf == 1):
+        plot(errors, "Zhang")
+    return x, iteracion
 
 
-print(sne_fd_3(-0.5, 0.000001, "lambda x: x**2 - math.e**x-3*x+2"))
+
+# print(sne_fd_3(-0.5, 0.000001, "lambda x: x**2 - math.e**x-3*x+2"))
 #print(sne_fd_3(2, 0.00000000000001, "lambda x: math.atan(x)"))
 def plot_f(errors,values):
     plt.plot(values, errors)
@@ -87,3 +150,9 @@ def plot_f(errors,values):
     plt.xlabel("Iteraciones")
     plt.show()
     
+def plot(errors, title):
+    plt.plot(errors)
+    plt.title(title)
+    plt.ylabel("Error")
+    plt.xlabel("Iteraciones")
+    plt.show()
